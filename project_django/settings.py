5617742b812633ff058637178c4226e45f29b81b
navisraditya <navisraditya@gmail.com>
2022-09-29 14:17:53 +0700

    static_root changed

--- project_django/settings.py
+++ project_django/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join("/static/style.css/", 'static')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
