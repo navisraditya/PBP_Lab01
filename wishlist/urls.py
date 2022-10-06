b0b0099659173315bd52ba2253f3e9fae4ac19f5
navisraditya <navisraditya@gmail.com>
2022-10-06 14:26:43 +0700

    ajax attempted

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from wishlist.views import show_wishlist, show_xml, show_json, show_json_by_id, show_xml_by_id, register, login_user, logout_user
+from wishlist.views import show_wishlist, show_xml, show_json, show_json_by_id, show_xml_by_id, register, login_user, logout_user, show_wishlist_ajax
 
 app_name = 'wishlist'
 
 urlpatterns = [
     path('', show_wishlist, name='show_wishlist'),
+    path('ajax/', show_wishlist_ajax, name='show_wishlist_ajax'),
     path('xml/', show_xml, name='show_xml'),
     path('json/', show_json, name='show_json'),
     path('json/<int:id>', show_json_by_id, name='show_json_by_id'),
